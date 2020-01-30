785132d40eaa26bb588534f68dc92be62a5c68e6
Samvel Gevorgyan <saymoncool93@gmail.com>
2020-01-30 10:45:16 -0600

    Added my HM1

---
+++ scatter_normal.R
@@ -0,0 +1,8 @@
+library(nycflights13)
+library(ggplot2)
+library(dplyr)
+alaska_flights <- flights %>% 
+  filter(carrier == "AS") 
+ggplot(data = alaska_flights, mapping = aes(x = dep_delay, y = arr_delay)) + 
+  geom_point()
+ggsave("scatter_normal.pdf")
